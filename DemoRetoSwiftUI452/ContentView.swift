7fe57d33040ee9b56a56b3f4bec470d82bce4e09
Martin Molina <jose.molina@tec.mx>
2026-09-11 19:16:08 -0600

    Initial Commit

---
+++ DemoRetoSwiftUI452/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DemoRetoSwiftUI452
+//
+//  Created by José Molina on 11/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DemoRetoSwiftUI452/DemoRetoSwiftUI452App.swift
@@ -0,0 +1,17 @@
+//
+//  DemoRetoSwiftUI452App.swift
+//  DemoRetoSwiftUI452
+//
+//  Created by José Molina on 11/09/26.
+//
+
+import SwiftUI
+
+@main
+struct DemoRetoSwiftUI452App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
